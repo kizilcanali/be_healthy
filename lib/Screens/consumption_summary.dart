272a90f0e358a9dca68df215cfe056695bdcffb0
Ali Kızılcan <kizilcanali@gmail.com>
2021-05-03 00:01:03 +0300

    summary nutritions print on screen, will be re-design

--- lib/Screens/consumption_summary.dart
+++ lib/Screens/consumption_summary.dart
@@ -1,7 +1,5 @@
-import 'package:be_healthy/Widgets/custom_table_cell_item.dart';
 import 'package:be_healthy/Widgets/food_detail/expandable_summary_card.dart';
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 import '../state_management.dart';
 
@@ -43,6 +41,28 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
           return sumCal;
         }
 
+        List sumOfNutritionValues(List nutritionOfFoods) {
+          int sumProt = 0;
+          int sumCarb = 0;
+          int sumFat = 0;
+          List sumOfNutritions = [];
+
+          if (nutritionOfFoods.length != 0) {
+            //print(nutritionOfFoods);
+            for (int i = 0; i < nutritionOfFoods.length; i++) {
+              sumProt += nutritionOfFoods[i]["protein"];
+              sumCarb += nutritionOfFoods[i]["carhbohydrate"];
+              sumFat += nutritionOfFoods[i]["fat"];
+            }
+            sumOfNutritions.add(sumProt);
+            sumOfNutritions.add(sumCarb);
+            sumOfNutritions.add(sumFat);
+          }
+          //print("Sum $sumOfNutritions");
+          //print("Normal Liste: $nutritionOfFoods");
+          return sumOfNutritions;
+        }
+
         //Summary page cards.
         return ExpandableSummaryCard(
           dateText: sumFoods[index]["date"],
@@ -50,6 +70,11 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
           totalCal: sumOfSummaryCalories(
             sumFoods[index]["foods"],
           ),
+          sumOfNutritionValues: [
+            sumOfNutritionValues(
+              sumFoods[index]["foods"],
+            )
+          ],
         );
       },
     );
--- lib/widgets/food_detail/expandable_summary_card.dart
+++ lib/widgets/food_detail/expandable_summary_card.dart
@@ -3,19 +3,22 @@ import 'package:lottie/lottie.dart';
 import '../custom_table_cell_item.dart';
 
 class ExpandableSummaryCard extends StatelessWidget {
-  String dateText;
-  List foods;
-  int totalCal;
-  ExpandableSummaryCard({this.dateText, this.foods, this.totalCal});
+  final String dateText;
+  final List foods;
+  final int totalCal;
+  final List sumOfNutritionValues;
+  ExpandableSummaryCard(
+      {this.dateText, this.foods, this.totalCal, this.sumOfNutritionValues});
 
   @override
   Widget build(BuildContext context) {
+    //print(sumOfNutritionValues);
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 40),
+      padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(25),
         child: ExpansionTile(
-          //childrenPadding: EdgeInsets.symmetric(vertical: 20),
+          //childrenPadding: EdgeInsets.symmetric(vertical: 10),
           backgroundColor: Colors.white,
           collapsedBackgroundColor: Colors.white,
           title: Row(
@@ -32,7 +35,6 @@ class ExpandableSummaryCard extends StatelessWidget {
                     Text(
                       dateText,
                       overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
                       style:
                           TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
                     ),
@@ -41,7 +43,6 @@ class ExpandableSummaryCard extends StatelessWidget {
                     ),
                     Text(
                       totalCal.toString() + " kcal",
-                      //sumOfSummaryCalories(calories).toString(),
                       style: TextStyle(
                           fontSize: 16,
                           fontWeight: FontWeight.bold,
@@ -53,42 +54,38 @@ class ExpandableSummaryCard extends StatelessWidget {
             ],
           ),
           children: [
-            ListView.builder(
-              shrinkWrap: true,
-              //padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
-              itemCount: foods.length,
-              //scrollDirection: Axis.vertical,
-              itemBuilder: (BuildContext context, int index) {
-                return Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Table(
-                    columnWidths: {
-                      0: FlexColumnWidth(3),
-                      1: FlexColumnWidth(2)
-                    },
-                    defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-                    children: [
-                      TableRow(
+            Column(
+              children: [
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: foods.length,
+                  //scrollDirection: Axis.vertical,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      title: Column(
                         children: [
-                          CustomTableCellItem(
-                            text: foods[index]["name"],
-                            customTextStyle: TextStyle(
-                                fontSize: 17, fontWeight: FontWeight.w500),
-                          ),
-                          CustomTableCellItem(
-                            text: foods[index]["calorie"].toString() + " kcal",
-                            customTextStyle: TextStyle(
-                              fontSize: 14,
-                              color: Color(0xFFFA4A0C),
-                              fontWeight: FontWeight.bold,
-                            ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                foods[index]["name"],
+                              ),
+                              Text(
+                                foods[index]["calorie"].toString() + " kcal",
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                    ],
-                  ),
-                );
-              },
+                    );
+                  },
+                ),
+                Row(children: [
+                  Text("P: ${sumOfNutritionValues[0][0]} g"),
+                  Text("C: ${sumOfNutritionValues[0][1]} g"),
+                  Text("F: ${sumOfNutritionValues[0][2]} g"),
+                ])
+              ],
             ),
           ],
         ),
